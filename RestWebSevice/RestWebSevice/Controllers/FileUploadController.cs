f0a23b85942d20e86a1e605e31fe8849828ba886
josuecm13 <josuecanales0@gmail.com>
2017-11-06 14:07:51 -0600

    Dummy Service

---
+++ RestWebSevice/RestWebSevice/Controllers/FileUploadController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Threading.Tasks;
+using RestWebSevice.Extensions;
+
+namespace RestWebSevice.Controllers
+{
+
+    [RoutePrefix("api")]
+    public class FileUploadController : ApiController
+    {
+        [Route("upload")]
+        [HttpPost]
+        public async Task<HttpResponseMessage> UploadFile(HttpRequestMessage request)
+        {
+            if (!request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            var data = await Request.Content.ParseMultipartAsync();
+
+            if (data.Files.ContainsKey("file"))
+            {
+                var file = data.Files["file"].File;
+                var fileName = data.Files["file"].Filename;
+            }
+
+            if (data.Fields.ContainsKey("description"))
+            {
+                var description = data.Fields["description"].Value;
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent("Thank you for uploading the file...")
+            };
+        }
+    }
+}
